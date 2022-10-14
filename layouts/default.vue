b40b7200a60ca7f0c84297537c03cb3ff2b1fe90
WhiteNoise0000 <46626065+WhiteNoise0000@users.noreply.github.com>
2022-10-14 22:49:26 +0900

    「こんにちは！」ページ追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -106,6 +106,11 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
+        },
+        {
+          icon: 'mdi-apps',
+          title: 'こんにちは！',
+          to: '/hello'
         }
       ],
       miniVariant: false,
---
+++ pages/hello.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-btn color="primary">Test</v-btn>
+</template>
+
+<script>
+export default {
+    name: 'Hello Nuxt'
+}
+</script>
